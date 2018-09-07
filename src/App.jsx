1b92daeb1309d49db6c02734a48951d257a5c03b
Adam <adamqkc@gmail.com>
2018-09-06 17:15:42 -0700

    refactor chatbar.jsx functions

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,15 @@ class App extends Component {
     super();
     this.state = {
       currentUser: {name: 'Bob'},
-      messages: [],
+      messages: [{
+        username: 'Bob',
+        content: 'Hi Michelle'
+      }]
     }
-    this.sendMessage = this.sendMessage.bind(this);
-    this.changeName = this.changeName.bind(this);
+    // this.sendMessage = this.sendMessage.bind(this);
+    // this.sendNotification = this.sendNotification.bind(this);
   }
+
 
   componentDidMount() {
     this.socket = new WebSocket('ws://localhost:3001')
@@ -20,47 +24,54 @@ class App extends Component {
     this.socket.onopen = function() {
       console.log('Client connected to server!')
     }
-    
-    this.socket.onmessage = (data) => {
-      this.saveMessage(data);
-    }
 
+    // this.socket.onmessage = (data) => {
+    //   // const newMessage = JSON.parse(data.data)
+    //   // if (newMessage.type === 'message') {
+    //   //   this.saveMessage(newMessage);
+    //   // } else if (newMessage.type === 'notification') {
+    //   //   this.saveName(newMessage);
+    //   // }
+    // }
+    
+    
     setTimeout(() => {
       const newMessage = {username: 'Michelle', content: 'Hello there!'};
       const messages = this.state.messages.concat(newMessage)
-
       this.setState({ messages: messages })
     }, 1000);
   }
+  
 
-  sendMessage(event) {
-    if (event.key === 'Enter') {
-      const contentField = event.target;
-      const newMessage = {
-        type: 'message', 
-        username: this.state.currentUser.name,
-        content: contentField.value,
-      }
-      contentField.value = '';
-      this.socket.send(JSON.stringify(newMessage));
-    }
+  sendMessage(newMessage) {
+    console.log(newMessage)
+    // this.socket.send(JSON.stringify(newMessage));
   }
+  
+  
+  // saveMessage(newMessage) {
+    //   const oldMessages = this.state.messages;
+    //   const newMessages = [...oldMessages, newMessage];
+    //   this.setState({ messages: newMessages });
+    // }
+    
+    
+  sendNotification(newNotification) {
+    console.log(newNotification)
+    // this.socket.send(JSON.stringify(newNotification);
+  }
+  
+  // saveName(newNotification) {
+  //   const oldMessages = this.state.messages;
+  //   const newNotifications = [...oldMessages, newNotification];
+  //   console.log(newNotification.username);
 
-  saveMessage(message) {
-    const newMessage = JSON.parse(message.data)
-    const oldMessages = this.state.messages;
-    const newMessages = [...oldMessages, newMessage];
-    this.setState({ messages: newMessages });
-  }
+  //   this.setState({ 
+  //     currentUser: newNotification.username,
+  //     messages: newNotifications
+  //   });
+  // }
 
-  changeName(event) {
-    if (event.key === 'Enter') {
-      let nameField = event.target;
-      const newName = { name: nameField.value }
-      nameField.value = '';
-      this.socket.send(JSON.stringify(newName));
-    }
-  }
 
   render() {
     return (
@@ -68,12 +79,12 @@ class App extends Component {
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-        
+
         <main className='messages'>
-          <MessageList messages={this.state.messages}/>
+          <MessageList messages={this.state.messages} />
         </main>
         
-        <ChatBar currentUser={this.state.currentUser} sendMessage={this.sendMessage} changeName={this.changeName}/>
+        <ChatBar currentUser={this.state.currentUser} sendMessage={this.sendMessage} sendNotification={this.sendNotification} />
       </div>
     );
   }
--- src/Message.jsx
+++ src/Message.jsx
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 
 class Message extends Component {
   render() {
+    const notification = <div className="notification">
+      <span className="notification-content">this.props.messages.content</span>
+    </div>
+
     return (
-      <div className="message">
-        <span className="message-username">{this.props.message.username}</span>
-        <span className="message-content">{this.props.message.content}</span>
+      <div>
+        <div className="message">
+          <span className="message-username">{this.props.message.username ? this.props.message.username : 'Anonymous'}</span>
+          <span className="message-content">{this.props.message.content}</span>
+        </div>
       </div>
     )
   }
